793b0bf76232efa4727cc07c0b1aeb9db8cff372
waghpavan <pavandagaduwagh@gmail.com>
2025-07-19 15:26:31 +0530

    Profile Updation

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 
-
+// "homepage" : "https://waghpavan.github.io/portfolio/",
 function App() {
   return (
     <div className="min-h-screen font-ysabeau transition-all duration-300 bg-white dark:bg-black text-black dark:text-white">
